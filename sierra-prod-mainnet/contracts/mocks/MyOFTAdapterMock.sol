681ec0e076ac3b55abcf6c70177534a75ad658be
Tom Niermann <124015207+tomniermann@users.noreply.github.com>
2025-10-15 11:43:33 -0700

    Deploy to mainnet

--- sierra-prod-mainnet/contracts/mocks/MyOFTAdapterMock.sol
+++ sierra-prod-mainnet/contracts/mocks/MyOFTAdapterMock.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { MyOFTAdapter } from "../MyOFTAdapter.sol";
+import { LytPoolOFTAdapter } from "../LytPoolOFTAdapter.sol";
 
 // @dev WARNING: This is for testing purposes only
-contract MyOFTAdapterMock is MyOFTAdapter {
-    constructor(address _token, address _lzEndpoint, address _delegate) MyOFTAdapter(_token, _lzEndpoint, _delegate) {}
+contract MyOFTAdapterMock is LytPoolOFTAdapter {
+    constructor(address _token, address _lzEndpoint, address _delegate) LytPoolOFTAdapter(_token, _lzEndpoint, _delegate) {}
 }
